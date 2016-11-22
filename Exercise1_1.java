a2d222ca44136cc24db3a388d558df617d28b379
gsiqi <g47@SiqideMacBook-Air.local>
2016-11-22 10:35:40 -0700

    try again

---
+++ Exercise1_1.java
@@ -0,0 +1,19 @@
+/*Name: Sophie Gao
+ * ID: 0098980
+ * Program:Exercise
+ * THis program will print three messages.
+ * Created by Sophie on 5/10/2016
+ */
+package chapter1;
+
+public class Exercise1_1 {
+
+	public static void main(String[] args) {
+		// print three messages
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+
+	}
+
+}
